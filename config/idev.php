5cb028cf34223bbfe02ba4f20aa4927aa538bde2
farchanw <35190412+farchanw@users.noreply.github.com>
2025-12-22 11:33:28 +0700

    update change theme

--- config/idev.php
+++ config/idev.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'theme' => 'default', // available : default, greeny, monochrome, bluesy, red
+    'theme' => 'bluesy', // available : default, greeny, monochrome, bluesy, red
     'app_name' => 'Sistem Produksi',
     // 'app_logo' => asset('easyadmin/idev/img/logo-idev.png'),
     // 'app_favicon' => asset('easyadmin/idev/img/favicon.png'),
